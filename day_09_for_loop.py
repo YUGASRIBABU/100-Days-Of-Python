3cf0b63f61270fd2d1733e2d19a6cb2c03c90819
YUGASRIBABU <yugasrib@gmail.com>
2026-06-15 10:34:05 +0530

    Day 9:Completed basic math pratical using for loop

---
+++ day_09_for_loop.py
@@ -0,0 +1,14 @@
+# Day 9: Basic Math For Loop Practice
+number = [2, 4, 6, 8]
+
+print("Starting our for loop Math Challenge!")
+print("-" * 40)
+
+for num in number:
+    squared = num ** 2
+    tripled = num * 3
+
+    print(f"Original Number: {num}")
+    print(f"-> Squared = {squared}")
+    print(f"-> Tripled = {tripled}")
+    print("-" * 40)
